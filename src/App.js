9cb9abbb286c8238c38e98057462ad6d970683e3
fatigi22 <fatihiayoubaa22@gmail.com>
2025-04-13 19:35:06 +0200

    creation footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Card from "./componemt/card";
+import Footer from "./componemt/Footer";
 import Navbar from "./componemt/Navbar";
 function App() {
   return (
    <>
 <Navbar />
 <Card />
+<Footer />
    </>
   );
 }
